95dc5b5f2348ea76ce28071df0beaaa1303295ff
Joanna Tzu-Hsuan Huang <joannathhuang@Joannas-MacBook-Pro.local>
2016-09-12 10:42:59 -0400

    Done

--- Pets.playground/Contents.swift
+++ Pets.playground/Contents.swift
@@ -1,18 +1,19 @@
-var petName: String?
-petName = "Scooter 🐢"
+var petName:String?
+//petName = "Scooter"
 print(petName)
 
 if petName != nil {
     print("My friend's pet is \(petName)")
 } else {
-    print("My friend doesn't have a pet 😞")
+    print("My friend doesn't have a pet :(")
 }
 
 if let petName = petName {
-    print("My friend's pet is \(petName)")
+    print ("My friend's pet is \(petName)")
 }
 
 if let friendsPet = petName {
-    print("My friend's pet is \(friendsPet)")
+    print ("My friend's pet is \(friendsPet)")
     print("petName is still an Optional! \(petName)")
 }
+
